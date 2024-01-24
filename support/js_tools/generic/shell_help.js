8dcaeb931f2cf4abfdb77a6daaaef62da90be071
Jeff Hykin <jeff.hykin@gmail.com>
2024-01-24 09:23:35 -0600

    -

--- support/js_tools/generic/shell_help.js
+++ support/js_tools/generic/shell_help.js
@@ -1,6 +1,7 @@
 import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.62/main/file_system.js"
 import { run, hasCommand, throwIfFails, zipInto, mergeInto, returnAsString, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, } from "https://deno.land/x/quickr@0.6.62/main/run.js"
 import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.62/main/console.js"
+import { streamToString } from "https://deno.land/x/quickr@0.6.62/main/stream_tools.js"
 
 import { capitalize, indent, toCamelCase, digitsToEnglishArray, toPascalCase, toKebabCase, toSnakeCase, toScreamingtoKebabCase, toScreamingtoSnakeCase, toRepresentation, toString, regex, findAll, iterativelyFindAll, escapeRegexMatch, escapeRegexReplace, extractFirst, isValidIdentifier, removeCommonPrefix } from "https://deno.land/x/good@1.6.0.0/string.js"
 
@@ -18,9 +19,10 @@ export const extractEnvVarsFromShellScript = async ({ scriptPath, shellExecutabl
         console.debug(`scriptPath is:`,scriptPath)
         let scriptContent = FileSystem.sync.read(scriptPath)
         console.debug(`scriptContent is:`, indent({ string: scriptContent}))
-        const output = await run(
-            shellExecutable,
-            "-c",
+        let command = new Deno.Command(shellExecutable, {
+            args: [
+                "-c",
+            ],
             `
                 . '${shellPartEscape(scriptPath)}'
                 
@@ -28,9 +30,22 @@ export const extractEnvVarsFromShellScript = async ({ scriptPath, shellExecutabl
                 '${shellPartEscape(Deno.execPath)}' eval '${shellPartEscape(`console.log(JSON.stringify(Deno.env.toObject()))`)}'
                 echo end of deno output
             `,
-            Stdout(returnAsString)
-        )
-        console.debug(`output is:`, indent({ string: output}))
+        })
+        let process = command.spawn()
+        const output = await streamToString(process.stdout)
+        
+        // const output = await run(
+        //     shellExecutable,
+        //     "-c",
+        //     `
+        //         . '${shellPartEscape(scriptPath)}'
+                
+        //         echo "${endIdentifierString}"
+        //         '${shellPartEscape(Deno.execPath)}' eval '${shellPartEscape(`console.log(JSON.stringify(Deno.env.toObject()))`)}'
+        //         echo end of deno output
+        //     `,
+        //     Stdout(returnAsString)
+        // )
         const pattern = new RegExp(`([^a]|[a])*${endIdentifierString}`)
         if (!output.match(pattern)) {
             if (debug) {
